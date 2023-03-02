594294ac3161287727ee2f7db55c897283478595
Andrew Wang <90523572+WA287142@users.noreply.github.com>
2023-03-02 14:55:23 -0600

    Added Server.py and EtherNet/IP.py
    Server.py is used to host the server that will be used to transfer data from Unreal Engine to the Nanotec motor controller.
    EtherNet/IP.py is the script that uses a Python wrapper of EtherNet/IP to connect to the motor controller and make calls to the motor.

---
+++ etherNetIP.py
@@ -0,0 +1,27 @@
+import socket
+from pycomm3 import LogixDriver
+
+######### This file requires a server to be running prior to running this file ################
+######### Need to run Server.py on a separate terminal to host the server ##################
+
+
+
+# create a socket connection to the server
+host = 'localhost'
+port = 7000
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
+
+# create an instance of the driver for the Nanotec motor controller
+ip_addy = '192.168.1.100'
+with LogixDriver(ip_addy) as plc:
+    # read a tag from the device
+    tag_value = plc.read_tag('MyTag')
+
+    # send the tag value over the socket connection
+    sock.sendall(str(tag_value).encode())
+
+
+
+# close the socket connection
+sock.close()
---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+
+try:
+    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # AF_INET is the address-family ipv4
+        # SOCK_STREAM is connection oriented TCP protocol
+except socket.error as err:
+    print("Server creation failed: %s" %(err))
+
+# set the socket options to allow multiple connections
+serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# ip = socket.gethostbyname('www.google.com')
+#     # obtaining the ip address
+# print(ip)
+
+port = 7000   # Default port for socket
+
+serv.bind(('127.0.0.1', port))
+print("socket binded to %s" %(port))
+
+serv.listen(5)  # Listen for connections made to the server. The 5 is the max number of connections
+print("socket listen")
+
+
+while True:
+    conn, addr = serv.accept()  # accepting a connection request from client
+        # addr stores the address from which the connection came from
+    print('Got connection from ', addr)
+
+    conn.sendall("Sending encoded byte type confirmation message from SERVER".encode())
+
+    from_client = ''
+
+    # while True:
+    #     data = conn.recv(4096)  # 4096 is number of bytes transferred. Might be an issue
+    #     if not data:
+    #         break
+    #     from_client += data
+    #     print(from_client)
+
+    #     conn.send("Message from Server\n")
+
+    while True:
+        data = conn.recv(4096)
+        if not data:
+            break
+        
+        print ("Client says: "+data.decode())
+
+        conn.sendall('Server received message'.encode())
+
+
+    conn.close()
+    print('client disconnected')
+
+    break
