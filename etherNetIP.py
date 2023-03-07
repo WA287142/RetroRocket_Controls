1e39ff2cfdd429eb0632e58351c77a8f4448bf87
Wang <wa287142@tamu.edu>
2023-03-07 17:28:31 -0600

    tested IP address connection

--- etherNetIP.py
+++ etherNetIP.py
@@ -5,23 +5,38 @@ from pycomm3 import LogixDriver
 ######### Need to run Server.py on a separate terminal to host the server ##################
 
 
-
+print("TEst")
 # create a socket connection to the server
-host = 'localhost'
-port = 7000
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host, port))
+# host = 'localhost'
+# port = 7000
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.connect((host, port))
 
 # create an instance of the driver for the Nanotec motor controller
-ip_address = '192.168.1.100'
-with LogixDriver(ip_address) as plc:
-    # read a tag from the device
-    tag_value = plc.read_tag('MyTag')
+ip_address = '169.254.89.103'
+print("IP Address")
+# with LogixDriver(ip_address) as plc:
+#     print("LogixDriver")
 
-    # send the tag value over the socket connection
-    sock.sendall(str(tag_value).encode())
+#     tags = plc.read('MyTag1', 'MyTag2', 'MyTag3')
+#     print(tags)
+#     # Get the tag list
+#     plc.read('dint_tag')
 
+#     # read a tag from the device
+#     #tag_value = plc.read_tag('MyTag')
 
+#     # send the tag value over the socket connection
+#     #sock.sendall(str(tag_value).encode())
+
+plc = LogixDriver(ip_address, 44818)
+
+plc.open()
+print("LogixDriver made")
+
+plc.get_tag_list()    
+
+plc.close()
 
 # close the socket connection
-sock.close()
+#sock.close()
